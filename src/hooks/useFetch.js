ac4d502655b5c59381b76541aba5c1d118dc2086
Mike Guijarro <a01612042@itesm.mx>
2022-04-17 22:54:08 -0400

    new useFetch implemented, now accepts fetchOptions as a param

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+// Inspirations for this hook:
+// https://gist.github.com/simicd/bbf37fe119c7b344634e4d47d2709fea
+
+export const useFetch = (url, fetchOptions, processData) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
+
+    let counter = useRef(0);
+
+    // If no processing function is passed just return the data
+    // The callback hook ensures that the function is only created once
+    // and hence the effect hook below doesn't start an infinite loop
+    const processJson = useCallback(
+        processData || ((jsonBody) => (jsonBody)),
+        []);
+
+    // Turn objects into strings for useCallback & useEffect dependencies
+    const stringifiedUrl = JSON.stringify(url);
+    const stringifiedFetchOpt = JSON.stringify(fetchOptions);
+
+    useEffect(() => {
+        counter.current++;
+        const controller = new AbortController();
+
+        const fetchData = async () => {
+            try {
+                setIsPending(true);
+
+                const res = await fetch(url, { ...fetchOptions, signal: controller.signal });
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                const resData = await res.json();
+                const processedData = processJson(resData);
+                setIsPending(false);
+                setData(processedData);
+                setError(null);
+            } catch (e) {
+                if (e.name === 'AbortError') {
+                    console.log('The fetch was aborted');
+                } else {
+                    setIsPending(false);
+                    setError('Could not fetch data...');
+                }
+            }
+        };
+
+        fetchData();
+
+        // Cleanup function for unmouting phase
+        return () => {
+            console.log('Unmounted hooks')
+            controller.abort();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [stringifiedUrl, stringifiedFetchOpt, processData]);
+
+    return { data, isPending, error };
+}
